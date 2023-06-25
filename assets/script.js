14a86949a8a96064199a2d65f2e4a3536081f0fb
Hazkeel Qureshi <hazkeel27@gmail.com>
2023-06-25 09:21:10 -0400

    third question is complete and fourth question's code is written

--- assets/script.js
+++ assets/script.js
@@ -160,10 +160,6 @@ function init(){
                 //secondQuestion div is appended to firstQuestion div
                 firstQuestion.appendChild(secondQuestion);
 
-
-
-
-
                // function runs on answer button click event and takes user to question 3
                 secondQuestion.addEventListener('click', function(event){
 
@@ -187,7 +183,7 @@ function init(){
                         result.textContent = 'Incorrect';
                     }
                     
-                    //firstQuestion div's child elements are cleared
+                    //secondQuestion div's child elements are cleared
                     secondQuestion.innerHTML = '';
 
                     //new div for the second question is created within firstQuestion div
@@ -235,16 +231,99 @@ function init(){
                     secondQuestion.appendChild(thirdQuestion);
 
 
+
+
+
+
+                    // function runs on answer button click event and takes user to question 4
+                    thirdQuestion.addEventListener('click', function(event){
+
+                    event.stopPropagation();
+    
+                    var thirdAnswer = event.target;
+    
+                    //checks if the button element is clicked
+                    if (thirdAnswer.matches('button')){
+    
+                        //the button's text content is assigned to variable
+                        thirdAnswer = thirdAnswer.textContent;
+                        
+                        console.log(thirdAnswer);
+    
+                        //checks the answer and appends the result
+                        if (thirdAnswer === 'curly brackets'){
+                            result.textContent = 'Correct';
+                        }
+                        else{
+                            result.textContent = 'Incorrect';
+                        }
+                        
+                        //thirdQuestion div's child elements are cleared
+                        thirdQuestion.innerHTML = '';
+    
+                        //new div for the second question is created within firstQuestion div
+                        var fourthQuestion = document.createElement('div');
+    
+                        //h1 tag is edited, and appended
+                        mainHeader.textContent = 'Commonly used data types DO NOT include';
+                        fourthQuestion.appendChild(mainHeader);  
+    
+                        //answer options are added to button tag's text content
+                        answerButton1.textContent = 'strings';
+                        answerButton2.textContent = 'booleans';
+                        answerButton3.textContent = 'alerts';
+                        answerButton4.textContent = 'numbers';
+    
+                        //button tags are appended to the li tags
+                        listItem1.appendChild(answerButton1);
+                        listItem2.appendChild(answerButton2);
+                        listItem3.appendChild(answerButton3);
+                        listItem4.appendChild(answerButton4);
+    
+                        //li tags are appended to ol tag
+                        orderedList.appendChild(listItem1);
+                        orderedList.appendChild(listItem2);
+                        orderedList.appendChild(listItem3);
+                        orderedList.appendChild(listItem4);
+    
+                        //ol tag is appended to thirdQuestion div
+                        fourthQuestion.appendChild(orderedList); 
+    
+                        //result of the second question is appended
+                        fourthQuestion.appendChild(result);
+    
+                        //shows the result on screen for 2 seconds
+                        resultSeconds = 2;
+                        resultTimer = setInterval(function(){
+                            resultSeconds--;
+                            if (resultSeconds === 0){
+                                clearInterval(resultTimer);
+                                result.textContent = '';
+                            }
+                        }, 1000);
+    
+                        //fourthQuestion div is appended to thirdQuestion div
+                        thirdQuestion.appendChild(fourthQuestion);
+    
+    
+                    } //if the button element is clicked ends
+                }); //question 4 code ends
+                
+
+
+
+
+
+
+
+
+
                 } //if the button element is clicked ends
             }); //question 3 code ends
 
-
-
-
-
-
             } //if the button element is clicked ends
         }); //question 2 code ends
+
     }); //start button click event and question 1 code ends
 } //init() ends
 
